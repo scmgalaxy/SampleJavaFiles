4538e931541aa453d390b6b58a8894e57eba6ed2
unknown <rajesh@scmgalay.com>
2015-10-03 18:58:43 +0530

    this is my first version of file

---
+++ main/My.java
@@ -0,0 +1,13 @@
+package com.companyname.software;
+
+/**
+ * Hello world!
+ *
+ */
+public class My 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
